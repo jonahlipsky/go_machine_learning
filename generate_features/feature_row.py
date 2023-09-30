e3d929a62c5ca8d201a41efeceb1743307a82bd4
Jonah Lipsky <jonahlipsky@gmail.com>
2023-09-30 15:12:50 -0400

    double check feature rows

--- generate_features/feature_row.py
+++ generate_features/feature_row.py
@@ -12,8 +12,9 @@ class FeatureRow:
         if version == 'v1':
             # num captures b/w, is_blacks_move (satisfies white too), and current turn
             num_non_board_related_features = 4
-            
-            return (board_size ^ 2) * 3 + num_ranks + num_non_board_related_features
+            board_size_move_features = (board_size ** 2) * 2
+            target_features = (board_size ** 2) + 1 #one for each spot plus one for no move
+            return board_size_move_features + target_features + num_ranks + num_non_board_related_features
         else:
             raise Exception('Not implemented.')
         
@@ -21,18 +22,26 @@ class FeatureRow:
     def feature_headers(cls, version, board_size):
         headers = []
         if version == 'v1':
+            for color in ['b', 'w']:
+                for row in range(board_size):
+                    for col in range(board_size):
+                        headers.append(f"feat_{color}_at_{row}_{col}")
+            for color in ['b', 'w']:
+                for dan_rank in range(9, 0, -1):
+                    headers.append(f"feat_{color}_ranked_{dan_rank}d")
+                for kyu_rank in range(30, 0, -1):
+                    headers.append(f"feat_{color}_ranked_{kyu_rank}k")
+            for color in ['b', 'w']:
+                 headers.append(f"feat_{color}_captures")
+            
+            headers.append(f"feat_is_blacks_move")
+            headers.append("feat_turn_count")
+
             for row in range(board_size):
                 for col in range(board_size):
-                    headers.append(f"({row},{col})")
-            for dan_rank in range(9, 0, -1):
-                headers.append(f"{dan_rank}d")
-            for kyu_rank in range(30, 0, -1):
-                headers.append(f"{kyu_rank}k")
-            for color in ['b', 'w']:
-                 headers.append(f"{color}_captures")
-            
-            headers.append(f"is_blacks_move")
-            headers.append("turn_count")
+                    headers.append(f"target_{row}_{col}_next_move")
+            headers.append('target_no_move')
+            return headers
         else:
             raise Exception(f"Not implemented for version: {version}")
 
@@ -45,23 +54,19 @@ class FeatureRow:
         board = Board(self.game.get_size())
         feature_rows = []
         game_sequence = self.game.get_main_sequence()
-        for i, node in enumerate(game_sequence):
-            row = []
-            if i == 0:
-
+        # for i, node in enumerate(game_sequence):
+        #     row = []
             
-            color, coords = node.get_move()
-            
-            if i == 1 and color != "b":
-                raise Exception("Check your assumption that i == 0 is always a non-move.")
+        #     if i == 1 and color != "b":
+        #         raise Exception("Check your assumption that i == 0 is always a non-move.")
 
 
 
-            if coords is None:
-                continue
+        #     if coords is None:
+        #         continue
 
-        for row in range(game.get_size()):
-            for col in range(game.get_size()):
+        # for row in range(game.get_size()):
+        #     for col in range(game.get_size()):
 
         
 

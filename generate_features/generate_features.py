f48e7ddec4aa47b21e204a293ce002f339c23c71
Jonah Lipsky <jonahlipsky@gmail.com>
2023-10-01 13:35:52 -0400

    update gitignore, add jupyter notebook, add combine csvs utility

--- generate_features/generate_features.py
+++ generate_features/generate_features.py
@@ -15,10 +15,7 @@ class GenerateFeatures:
     def fetch_all_game_file_names(self):
         games = []
         if os.path.isdir(self.pathway):
-            print('pathway')
             games = os.listdir(self.pathway)
-        else:
-            print('not a pathway')
         
         return games
     
@@ -74,21 +71,18 @@ class GenerateFeatures:
 
 
 if __name__ == '__main__':
-    year = 2006
-    month = 11
-    day = 5
+    for year in range(2007, 2009): 
+        for month in range(1,13):
+            for day in range(1,32):
+                if day < 10:
+                    day_str = '0' + str(day)
+                if month < 10:
+                    month_str = '0' + str(month)
+                year_str = str(year)
 
-    for month in range(1,13):
-        for day in range(1,32):
-            if day < 10:
-                day_str = '0' + str(day)
-            if month < 10:
-                month_str = '0' + str(month)
-            year_str = str(year)
-
-            if os.path.isdir(f"generate_features/sgfs-by-date/{year_str}/{month_str}/{day_str}"):
-                GF = GenerateFeatures(year_str, month_str, day_str, 9)
-                GF.fetch_and_store_game_features_and_targets()
-            else:
-                print('No games.')
+                if os.path.isdir(f"generate_features/sgfs-by-date/{year_str}/{month_str}/{day_str}"):
+                    GF = GenerateFeatures(year_str, month_str, day_str, 9)
+                    GF.fetch_and_store_game_features_and_targets()
+                else:
+                    print('No games.')
 
---
+++ utilities/combine_csvs.py
@@ -0,0 +1,26 @@
+from csv import writer
+import os
+
+def combine_csvs(csv_name_list, output_csv_name, has_headers=True):
+    prefix = os.getcwd()
+    with open(prefix + "/" + output_csv_name, 'a') as write_f_object:
+        writer_obj = writer(write_f_object)
+        for i, csv_name in enumerate(csv_name_list):
+            with open(csv_name, 'r') as read_f:
+                for j, line in enumerate(read_f):
+                    if has_headers and j == 0 and i != 0:
+                        continue # only write the headers from the first CSV
+
+                    writer_obj.writerow(line.split(','))
+            read_f.close()
+        write_f_object.close()
+
+if __name__ == '__main__':
+    names = [
+        'size_9_games/v1/2005/2005-features.csv',
+        'size_9_games/v1/2006/2006-features.csv',
+        'size_9_games/v1/2007/2007-features.csv',
+        'size_9_games/v1/2008/2008-features.csv',
+    ]
+    output_csv = '/size_9_games/v1/2005-2008.csv'
+    combine_csvs(names, output_csv)
